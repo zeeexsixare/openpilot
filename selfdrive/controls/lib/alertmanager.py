3e8fe1443d3c4d9246741a747adc7d8ab20303db
zeeexsixare <32753720+zeeexsixare@users.noreply.github.com>
2018-10-01 17:26:55 -0400

    Multi Breakpoint Moraga-style

--- selfdrive/controls/lib/alertmanager.py
+++ selfdrive/controls/lib/alertmanager.py
@@ -154,7 +154,7 @@ class AlertManager(object):
 
     "startup": Alert(
         #"Be ready to take over at any time",
-        "ret.steerKpV, ret.steerKiV = [[0.3], [0.01]]",
+        "Multi Breakpoint Moraga-style",
         "Always keep hands on wheel and eyes on road",
         AlertStatus.normal, AlertSize.mid,
         Priority.LOW_LOWEST, None, None, 0., 0., 15.),
