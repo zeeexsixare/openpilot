3dd793d1d0609a4deb7900886bec7768f725c541
zeeexsixare <32753720+zeeexsixare@users.noreply.github.com>
2018-10-02 07:48:42 -0400

    ret.steerKpV, ret.steerKiV = [[0.3], [0.02]]

--- selfdrive/controls/lib/alertmanager.py
+++ selfdrive/controls/lib/alertmanager.py
@@ -154,7 +154,7 @@ class AlertManager(object):
 
     "startup": Alert(
         #"Be ready to take over at any time",
-        "Multi Breakpoint Moraga-style",
+        "ret.steerKpV, ret.steerKiV = [[0.3], [0.02]]",
         "Always keep hands on wheel and eyes on road",
         AlertStatus.normal, AlertSize.mid,
         Priority.LOW_LOWEST, None, None, 0., 0., 15.),
