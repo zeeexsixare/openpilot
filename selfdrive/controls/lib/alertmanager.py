5dc1965f1c647d009ea7aad101ba0c7f2dbabb60
zeeexsixare <32753720+zeeexsixare@users.noreply.github.com>
2018-10-05 07:21:53 -0400

    KpV,KiV=[[0.1], [0.01]], Kf=0.00020

--- selfdrive/controls/lib/alertmanager.py
+++ selfdrive/controls/lib/alertmanager.py
@@ -154,7 +154,7 @@ class AlertManager(object):
 
     "startup": Alert(
         #"Be ready to take over at any time",
-        "ret.steerKpV, ret.steerKiV = [[0.2], [0.015]]",
+        "KpV,KiV=[[0.1], [0.01]], Kf=0.00020",
         "Always keep hands on wheel and eyes on road",
         AlertStatus.normal, AlertSize.mid,
         Priority.LOW_LOWEST, None, None, 0., 0., 15.),
