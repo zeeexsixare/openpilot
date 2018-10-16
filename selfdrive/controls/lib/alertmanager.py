d239cc28fe9014f795f1393d43f4f7a5e3bbf58b
zeeexsixare <32753720+zeeexsixare@users.noreply.github.com>
2018-10-16 18:17:34 -0400

    Inverted Moraga

--- selfdrive/controls/lib/alertmanager.py
+++ selfdrive/controls/lib/alertmanager.py
@@ -154,7 +154,7 @@ class AlertManager(object):
 
     "startup": Alert(
         #"Be ready to take over at any time",
-        "ret.steerKf = 0.00006",
+        "Inverted Moraga",
         "Always keep hands on wheel and eyes on road",
         AlertStatus.normal, AlertSize.mid,
         Priority.LOW_LOWEST, None, None, 0., 0., 15.),
