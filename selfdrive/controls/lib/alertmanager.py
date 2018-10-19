09ac9b0ef58c27fd24119d83c2938d58c573149e
zeeexsixare <32753720+zeeexsixare@users.noreply.github.com>
2018-10-19 07:34:05 -0400

    ret.steerActuatorDelay = 0.3

--- selfdrive/controls/lib/alertmanager.py
+++ selfdrive/controls/lib/alertmanager.py
@@ -154,7 +154,7 @@ class AlertManager(object):
 
     "startup": Alert(
         #"Be ready to take over at any time",
-        "Inverted Moraga",
+        "ret.steerActuatorDelay = 0.3",
         "Always keep hands on wheel and eyes on road",
         AlertStatus.normal, AlertSize.mid,
         Priority.LOW_LOWEST, None, None, 0., 0., 15.),
